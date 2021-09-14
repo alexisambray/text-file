be5fcd4c4ef1422d11991ed1a19d0c667e3b6aad
Alexis Ambray <alexisambray@gmail.com>
2021-09-14 20:35:33 +0800

    Example program for writing into a text file

---
+++ fwrite-1.c
@@ -0,0 +1,19 @@
+/*
+        Example program for writing into a text file.
+
+        "w" write mode -- but this is destructive!  It will overwrite the
+   original contents of a file if it exists.
+
+        "a" append mode - is not destructive.  It will write new at the end of
+   original file it exists.
+*/
+#include <stdio.h>
+int main() {
+  FILE* fp;
+
+  fp = fopen("sample.txt", "w");
+  fprintf(fp, "Hello world!\n");
+  fclose(fp);
+
+  return 0;
+}
